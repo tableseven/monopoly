702f98de5b544ecdefeb84d8df4d4e7be2798804
Shaeen <shaeenkevinsingh@gmail.com>
2020-04-23 13:13:25 +0200

    Adding comments to ai player script

--- assets/scripts/aiPlayer.js
+++ assets/scripts/aiPlayer.js
@@ -1,29 +1,40 @@
+/**
+ * Simple AI Player to simulate an opponent.
+ */
 function AIPlayer(p) {
   this.alertList = ''
 
+  // This variable is static, it is not related to each instance.
   this.constructor.count++
 
   p.name = 'AI ' + this.constructor.count
 
+  /**
+   * Decide whether to buy a property the AI landed on.
+   * Return: boolean (true to buy).
+   * Arguments:
+   * index: the property's index (0-39).
+   */
   this.buyProperty = function (index) {
-    console.log('buyProperty')
     var s = square[index]
 
-    if (p.money > s.price + 50) {
-      return true
-    } else {
-      return false
-    }
+    if (p.money > s.price + 50) return true
+    else return false
   }
 
+  /**
+   * Determine the response to an offered trade.
+   * Return: boolean/instanceof
+   * Trade: a valid Trade object to counter offer (with the AI as the recipient); false to decline; true to accept.
+   * Arguments:
+   * tradeObj: the proposed trade, an instanceof Trade, has the AI as the recipient.
+   */
   this.acceptTrade = function (tradeObj) {
-    console.log('acceptTrade')
-
-    var tradeValue = 0
-    var money = tradeObj.getMoney()
-    var initiator = tradeObj.getInitiator()
-    var recipient = tradeObj.getRecipient()
-    var property = []
+    var tradeValue = 0,
+      money = tradeObj.getMoney(),
+      initiator = tradeObj.getInitiator(),
+      recipient = tradeObj.getRecipient(),
+      property = []
 
     tradeValue += 10 * tradeObj.getCommunityChestJailCard()
     tradeValue += 10 * tradeObj.getChanceJailCard()
@@ -35,34 +46,31 @@ function AIPlayer(p) {
       tradeValue += tradeObj.getProperty(i) * square[i].price * (square[i].mortgage ? 0.5 : 1)
     }
 
-    console.log(tradeValue)
-
     var proposedMoney = 25 - tradeValue + money
 
-    if (tradeValue > 25) {
-      return true
-    } else if (tradeValue >= -50 && initiator.money > proposedMoney) {
+    if (tradeValue > 25) return true
+    else if (tradeValue >= -50 && initiator.money > proposedMoney)
       return new Trade(initiator, recipient, proposedMoney, property, tradeObj.getCommunityChestJailCard(), tradeObj.getChanceJailCard())
-    }
 
     return false
   }
 
+  /**
+   * This function is called at the beginning of the AI's turn, before any dice are rolled.
+   * The purpose is to allow the AI to manage property and/or initiate trades.
+   * Return: boolean: Must return true if and only if the AI proposed a trade.
+   */
   this.beforeTurn = function () {
-    console.log('beforeTurn')
-    var s
-    var allGroupOwned
-    var max
-    var leastHouseProperty
-    var leastHouseNumber
+    var s, allGroupOwned, max, leastHouseProperty, leastHouseNumber
 
+    // Buying houses.
     for (var i = 0; i < 40; i++) {
       s = square[i]
 
       if (s.owner === p.index && s.groupNumber >= 3) {
         max = s.group.length
         allGroupOwned = true
-        leastHouseNumber = 6
+        leastHouseNumber = 6 // No property will ever have 6 houses.
 
         for (var j = max - 1; j >= 0; j--) {
           if (square[s.group[j]].owner !== p.index) {
@@ -76,38 +84,38 @@ function AIPlayer(p) {
           }
         }
 
-        if (!allGroupOwned) {
-          continue
-        }
+        if (!allGroupOwned) continue
 
-        if (p.money > leastHouseProperty.houseprice + 100) {
-          buyHouse(leastHouseProperty.index)
-        }
+        if (p.money > leastHouseProperty.houseprice + 100) buyHouse(leastHouseProperty.index)
       }
     }
 
+    // Unmortgage property
     for (var i = 39; i >= 0; i--) {
       s = square[i]
 
-      if (s.owner === p.index && s.mortgage && p.money > s.price) {
-        unmortgage(i)
-      }
+      if (s.owner === p.index && s.mortgage && p.money > s.price) unmortgage(i)
     }
 
     return false
   }
 
-  var utilityForRailroadFlag = true
+  var utilityForRailroadFlag = true // Don't offer this trade more than once.
 
+  /**
+   * This function is called every time the AI lands on a square.
+   * The purpose is to allow the AI to manage property and/or initiate trades.
+   * Return: boolean: Must return true if and only if the AI proposed a trade.
+   */
   this.onLand = function () {
-    console.log('onLand')
-    var proposedTrade
-    var property = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    var railroadIndexes = [5, 15, 25, 35]
-    var requestedRailroad
-    var offeredUtility
-    var s
+    var proposedTrade,
+      property = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      railroadIndexes = [5, 15, 25, 35],
+      requestedRailroad,
+      offeredUtility,
+      s
 
+    // If AI owns exactly one utility, try to trade it for a railroad.
     for (var i = 0; i < 4; i++) {
       s = square[railroadIndexes[i]]
 
@@ -117,11 +125,8 @@ function AIPlayer(p) {
       }
     }
 
-    if (square[12].owner === p.index && square[28].owner !== p.index) {
-      offeredUtility = 12
-    } else if (square[28].owner === p.index && square[12].owner !== p.index) {
-      offeredUtility = 28
-    }
+    if (square[12].owner === p.index && square[28].owner !== p.index) offeredUtility = 12
+    else if (square[28].owner === p.index && square[12].owner !== p.index) offeredUtility = 28
 
     if (utilityForRailroadFlag && game.getDie(1) !== game.getDie(2) && requestedRailroad && offeredUtility) {
       utilityForRailroadFlag = false
@@ -137,42 +142,40 @@ function AIPlayer(p) {
     return false
   }
 
+  /**
+   * Determine whether to post bail/use get out of jail free card (if in possession).
+   * Return: boolean: true to post bail/use card.
+   */
   this.postBail = function () {
-    console.log('postBail')
-
-    if ((p.communityChestJailCard || p.chanceJailCard) && p.jailroll === 2) {
-      return true
-    } else {
-      return false
-    }
+    // p.jailroll === 2 on third turn in jail.
+    if ((p.communityChestJailCard || p.chanceJailCard) && p.jailroll === 2) return true
+    else return false
   }
 
+  /**
+   * Mortgage enough properties to pay debt.
+   * Return: void: don't return anything, just call the functions mortgage()/sellhouse()
+   */
   this.payDebt = function () {
-    console.log('payDebt')
     for (var i = 39; i >= 0; i--) {
       s = square[i]
 
-      if (s.owner === p.index && !s.mortgage && s.house === 0) {
-        mortgage(i)
-        console.log(s.name)
-      }
+      if (s.owner === p.index && !s.mortgage && s.house === 0) mortgage(i)
 
-      if (p.money >= 0) {
-        return
-      }
+      if (p.money >= 0) return
     }
   }
 
+  /**
+   * Determine what to bid during an auction.
+   * Return: integer: -1 for exit auction, 0 for pass, a positive value for the bid.
+   */
   this.bid = function (property, currentBid) {
-    console.log('bid')
     var bid
 
     bid = currentBid + Math.round(Math.random() * 20 + 10)
 
-    if (p.money < bid + 50 || bid > square[property].price * 1.5) {
-      return -1
-    } else {
-      return bid
-    }
+    if (p.money < bid + 50 || bid > square[property].price * 1.5) return -1
+    else return bid
   }
 }
